9b121522d8480906fa74f873e3435d8998a21e94
Tobias Buschor <tobias.buschor@shwups.ch>
2018-08-17 00:06:13 +0200

    Update src.js

--- src.js
+++ src.js
@@ -5,7 +5,7 @@
 	var listeners = [],
 		root = document.documentElement,
 		Observer;
-
+	if (!HTMLElement.prototype.matches) HTMLElement.prototype.matches = msMatchesSelector;
 	window.c1_onElement = function(selector, connectedCallback/*, disconnectedCallback*/) {
 		var listener = {
 			selector: selector,
